3eee72831121045dcb3d99f1f9590bedc719a6d2
Cerry <cerry0524@gmail.com>
2023-04-14 10:43:41 +0800

    新增運算子

--- index.php
+++ index.php
@@ -1,9 +1,45 @@
 <?php
 
-$name='mack';
+$name='Tom';
 $age=48;
 $total=10*100;
 
-echo "我的名字叫：".$name."<br>年紀是：".$age."<br>總共是：".$total;
-print "<hr><br>我的名字叫：".$name."<br>年紀是：".$age."<br>總共是：".$total;
+echo "<span style='color:blue;font-size:24px';>我的名字叫：".$name."</span>";
+echo "<br>年紀是：".$age;
+echo "<br>總共是：".$total."<br>";
+
+define("PI",3.1415);
+
+echo PI;
+echo "<br>";
+echo 10*10*PI;
+echo "<hr>";
+// 可變（動態）變數
+$name='mack';
+$$name=55;
+
+echo $mack;
+echo $$name;
+echo "<hr>";
+
+$num1=10;
+$num2=25;
+echo '$num1=' . $num1;
+echo "<br>";
+echo '$num2=' . $num2;
+echo "<br>";
+echo " + =>";
+echo $num1+$num2;
+echo "<br>";
+echo " - =>";
+echo $num1+$num2;
+echo "<br>";
+echo " * =>";
+echo $num1*$num2;
+echo "<br>";
+echo " / =>";
+echo $num1/$num2;
+echo "<br>";
+echo $num1%$num2;
+echo "<br>";
 ?>
