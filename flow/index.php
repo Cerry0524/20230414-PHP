9996f08e13d111752086dc7a84dd3e0e6bf3f22e
Cerry <cerry0524@gmail.com>
2023-04-14 14:27:51 +0800

    增加簡化語法及match用法

--- flow/index.php
+++ flow/index.php
@@ -1,12 +1,53 @@
 <?php
-$score=70;
+$score=90;
 
-if($score>60){
-    echo "成績＝".$score;
-    echo "<br>";
-    echo "恭喜及格了～～";
+if($score>=90){
+    $level="A";
 }else{
-    echo "成績＝".$score;
-    echo "<br>";
-    echo "請再接再厲～～";
+    if($score>=80){
+        $level="B";
+    }else{
+        if($score>=70){
+            $level="C";
+        }else{
+            if($score>=60){
+                $level="D";
+            }else{
+                $level="E";
+}}}}
+
+switch($level){
+    case "A":
+        echo "表現優良";
+        break;
+    case "B":
+        echo "值得肯定";
+        break;
+    case "C":
+        echo "需要練習";
+        break;
+    case "D":
+        echo "加強基本功";
+        break;
+    default:
+    echo "去死吧";
 }
+
+echo "<hr>";
+// 簡化語法
+if($score >=90)      {$level="A";
+}else if($score >=80){$level="B";
+}else if($score >=70){$level="C";
+}else if($score >=60){$level="D";
+}else                {$level="E";
+}
+// switch改成match
+$level = 59;
+$result = match(true){
+    $level >= 90 => "表現優良<br>",
+    $level >= 80 => "值得肯定<br>",
+    $level >= 70 => "需要練習<br>",
+    $level >= 60 => "加強基本功<br>",
+    default => "去死吧<br>"
+};
+echo $result;
