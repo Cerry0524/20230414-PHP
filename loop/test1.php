49973a58f865a69b87848fd321569af886a9f99e
Cerry <cerry0524@gmail.com>
2023-04-17 16:37:47 +0800

    TEST4更新

---
+++ loop/test1.php
@@ -0,0 +1,19 @@
+<?php
+$sold=900;
+if($sold >= 1000){
+    echo $sold * 0.8;
+}else{
+    echo $sold;
+}
+echo "<br>";
+$sold=1500;
+if($sold >= 1000){
+    echo $sold * 0.8;
+}
+echo "<br>";
+$sold=2300;
+if($sold >= 1000){
+    echo $sold * 0.8;
+}
+echo "<br>";
+echo "<hr>";
---
+++ loop/test2.php
@@ -0,0 +1,13 @@
+<?php
+$people = 100;
+switch ($people) {
+    case '<= 100':
+        echo "免費";
+        break;
+        case '>= 121';
+        echo "全票";  
+        break;  
+    default:
+        echo "半價";
+        break;
+}
