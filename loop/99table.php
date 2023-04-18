6cd5b2c6792520922e7e6832afe1dfb03c6e2283
Cerry <cerry0524@gmail.com>
2023-04-18 08:08:43 +0800

    新增九九乘法表表格版

--- loop/99table.php
+++ loop/99table.php
@@ -1,16 +1,18 @@
 <?php
-for ($i=1; $i <=9; $i++) { 
-    echo "   ".$i."   ";
+echo "<table border='1px solid black'>";
+for ($i=0; $i <=9; $i++) { 
+    echo "<th width='80px'>".$i."</th>";
 }
 echo "<br>";
 for ($i=1; $i <= 9 ; $i++) { 
-   
+    echo "<tr height='30px'>";
+    echo "<th>".$i."</th>";
 for ($j=1; $j <= 9 ; $j++) {
-    echo $i." ";
+    echo "<td>";    
     $result=$i * $j; 
     echo $i."*".$j."=".$result." ";
-    
+    echo "</td>";    
 }
-echo "<br>";
+echo "</tr>";
 }
 ?>
