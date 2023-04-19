253f5a0e03b1edd9aa8869a4e9467c4e804933a3
Cerry <cerry0524@gmail.com>
2023-04-19 16:18:38 +0800

    增加畫圖題目做一半

--- loop/test01.php
+++ loop/test01.php
@@ -1,16 +1,84 @@
 <?php
 // 直角三角形
-$i = 0;
-$j = 0;
-while ($i <= 4){
+echo "直角三角形";
+echo "<br>";
+for ($i=0; $i < 5; $i++) { 
     echo "*";
-    while ($j <= 4) {
-        echo "*";
-        while ($j <= $i) {
-            echo "<br>";     
-            $j += 1;
-        }
-        $i += 1;
+    for ($j=0; $j < $i; $j++) {
+    echo "*";
     }
+    echo "<br>";
 }
-  
+
+// 倒直角三角形
+echo "倒直角三角形";
+echo "<br>";
+for ($a=5; $a > 0; $a--) { 
+    echo "*";   
+    for ($b=$a-1; $b > 0; $b--) { 
+    echo "*";   
+    }
+    echo "<br>";
+}
+// 正三角形
+echo "正三角形";
+echo "<br>";
+$f = 5;
+
+for ($x=0; $x < $f; $x++) { 
+    echo "&nbsp&nbsp";
+       
+    for ($y=$x+1; $y < $f; $y++) { 
+        echo "&nbsp&nbsp";
+    }     
+    echo "*";
+    for ($z=0; $z < $x; $z++){
+        echo "**";
+    }
+echo "<br>";   
+}
+
+//菱形
+echo "菱形";
+echo "<br>";
+$f = 4;
+
+for ($x=0; $x < $f; $x++) { 
+    echo "&nbsp&nbsp&nbsp&nbsp";
+       
+    for ($y=$x+1; $y < $f; $y++) { 
+        echo "&nbsp&nbsp";
+    }     
+    echo "*";
+    for ($z=0; $z < $x; $z++){
+        echo "**";
+    }
+echo "<br>";  
+}
+$f = 5;
+for ($x=0; $x < $f; $x++) { 
+    echo "&nbsp&nbsp";
+       
+    for ($y=0; $y < $x; $y++) { 
+        echo "&nbsp&nbsp";
+    }     
+    echo "*";
+    for ($z=$x+1; $z < $f; $z++){
+        echo "**";
+    }
+echo "<br>";  
+}
+
+//矩型
+echo "矩型";
+echo "<br>";
+
+$f = 7;
+
+for($x=0; $x < $f; $x++){
+    echo "*";
+}
+echo "<br>";
+for ($y=0; $y < $f ; $y++) { 
+    echo "*";
+}
