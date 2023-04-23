c00c46bd6ffd126c7c464596f8bd9ae8fd958ca0
Cerry <cerry0524@gmail.com>
2023-04-24 07:56:24 +0800

    無法取得第四題重做成果

---
+++ array/homework04.php
@@ -0,0 +1,19 @@
+<?php
+$baseyear=1024;
+$sky[]=['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'];
+$earth[]=['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
+$skyearth=[];
+
+for($i=0;$i<60;$i++) {
+    $skyearth[]=[
+        "天干" => $sky[$i%10],
+        "地支" => $earth[$i%12]
+    ];
+}
+
+echo "<pre>";
+print_r($skyearth);
+echo "</pre>";
+
+$year=2099;
+echo $skyearth[($year-$baseyear)%60];
